1382130cb07cc83ecacfde5ba7192e2d05e329f6
jesserizzo <jesse.rizzo@gmail.com>
2018-08-24 17:10:08 -0500

    Add support for the original Envoy.

--- envoy_reader/__init__.py
+++ envoy_reader/__init__.py
@@ -1,1 +1,8 @@
-name = "envoy_reader"
+from envoy_reader.envoy_reader import production
+from envoy_reader.envoy_reader import consumption
+from envoy_reader.envoy_reader import daily_production
+from envoy_reader.envoy_reader import daily_consumption
+from envoy_reader.envoy_reader import seven_days_production
+from envoy_reader.envoy_reader import seven_days_consumption
+from envoy_reader.envoy_reader import lifetime_production
+from envoy_reader.envoy_reader import lifetime_consumption
---
+++ envoy_reader/envoy_reader.py
@@ -0,0 +1,158 @@
+import requests
+import sys
+import json
+
+envoy_model = "S"
+def call_api(ip_address):
+    url = "http://envoy/production.json".format(ip_address)
+    response = requests.get(url, timeout=5)
+    if response.status_code == 301:
+        global envoy_model
+        envoy_model = "Original"
+        url = "http://envoy/api/v1/production"
+        response = requests.get(url, timeout=5)
+        return response.json()
+
+    return response.json()
+
+
+def production(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            production = raw_json["production"][1]["wNow"]
+        else:
+            production = raw_json["wattsNow"]
+        return int(production)
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def consumption(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            consumption = raw_json["consumption"][0]["wNow"]
+            return int(consumption)
+        else:
+            return "Unavailable"
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def daily_production(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            daily_production = raw_json["production"][1]["whToday"]
+        else:
+            daily_production = raw_json["wattHoursToday"]
+        return int(daily_production)
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def daily_consumption(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            daily_consumption = raw_json["consumption"][0]["whToday"]
+            return int(daily_consumption)
+        else:
+            return "Unavailable"
+
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def seven_days_production(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            seven_days_production = raw_json["production"][1][
+                "whLastSevenDays"
+            ]
+        else:
+            seven_days_production = raw_json["wattHoursSevenDays"]
+        return int(seven_days_production)
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def seven_days_consumption(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            seven_days_consumption = raw_json["consumption"][0][
+                "whLastSevenDays"
+            ]
+            return int(seven_days_consumption)
+        else:
+            return "Unavailable"
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def lifetime_production(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            lifetime_production = raw_json["production"][1][
+                "whLifetime"
+            ]
+        else:
+            lifetime_production = raw_json["wattHoursLifetime"]
+        return int(lifetime_production)
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+def lifetime_consumption(ip_address):
+    try:
+        raw_json = call_api(ip_address)
+        if envoy_model == "S":
+            lifetime_consumption = raw_json["consumption"][0][
+                "whLifetime"
+            ]
+            return int(lifetime_consumption)
+        else:
+            return "Unavailable"
+
+    except requests.exceptions.ConnectionError:
+        return "Unable to connect to Envoy. Check the IP address"
+    except (json.decoder.JSONDecodeError, KeyError):
+        return ("Got a response, but it doesn't look right. " +
+                "Check the IP address")
+
+
+if __name__ == "__main__":
+    url = ""
+    print("production {}".format(production(url)))
+    print("consumption {}".format(consumption(url)))
+    print("daily_production {}".format(daily_production(url)))
+    print("daily_consumption {}".format(daily_consumption(url)))
+    print("seven_days_production {}".format(seven_days_production(url)))
+    print("seven_days_consumption {}".format(seven_days_consumption(url)))
+    print("lifetime_production {}".format(lifetime_production(url)))
+    print("lifetime_consumption {}".format(lifetime_consumption(url)))

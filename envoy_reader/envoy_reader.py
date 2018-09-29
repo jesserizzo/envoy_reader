cee7451453ddf35c0f5c193ea1dfe9d315b63fd6
David De Sloovere <david.desloovere@outlook.com>
2018-09-29 16:48:17 +0200

    Refactor endpoint check + error messages

--- envoy_reader/envoy_reader.py
+++ envoy_reader/envoy_reader.py
@@ -4,56 +4,66 @@ import json
 
 
 class EnvoyReader():
-    # C for Envoy model C - S for Envoy model S
-    model = ""
-    url = ""
+    # P for prooduction data only (ie. Envoy model C)
+    # PC for production and consumption data (ie. Envoy model S)
+    endpoint_type = ""
+    endpoint_url = ""
+
+    message_consumption_not_available = "Consumption data not available for your Envoy device."
 
     def __init__(self, host):
         self.host = host.lower()
 
     def detect_model(self):
-        self.url = "http://{}/production.json".format(self.host)
-        response = requests.get(self.url, timeout=10, allow_redirects=False)
+        self.endpoint_url = "http://{}/production.json".format(self.host)
+        response = requests.get(self.endpoint_url, timeout=10, allow_redirects=False)
         if response.status_code == 200 and len(response.json()) == 3:
-            self.model = "S"
+            self.endpoint_type = "PC"
+            return
         else:
-            self.url = "http://{}/api/v1/production".format(self.host)
-            response = requests.get(self.url, timeout=10, allow_redirects=False)
+            self.endpoint_url = "http://{}/api/v1/production".format(self.host)
+            response = requests.get(self.endpoint_url, timeout=10, allow_redirects=False)
             if response.status_code == 200:
-                self.model = "C"
+                self.endpoint_type = "P"
+                return
+
+        self.endpoint_url = ""
+        raise RuntimeError(
+                "Could not connect or determine Envoy model. " +
+                "Check that the device is up at 'http://" + self.host + "'.")
     
     def call_api(self):
-        # detection 
-        if self.model == "":
+        # detection of endpoint
+        if self.endpoint_type == "":
             EnvoyReader.detect_model(self)
 
-        if self.model == "S" or self.model == "C":
-            return EnvoyReader.call_api_get(self)
-        else:
-            raise RuntimeError("Could not connect or determine Envoy model. Check the IP address '" + self.host + "'.")
-
-    def call_api_get(self):
-        response = requests.get(self.url, timeout=10, allow_redirects=False)
+        response = requests.get(self.endpoint_url, timeout=10, allow_redirects=False)
         return response.json()
+
+    def create_json_errormessage(self):
+        return ("Unable to connect to Envoy. Check that the device is up at 'http://" + self.host + "'.")
+
+    def create_connect_errormessage(self):
+        return ("Got a response from '" + self.endpoint_url + "', but metric could not be found. " +
+                "Maybe your model of Envoy doesn't support the requested metric.")
 
     def production(self):
         try:
             raw_json = EnvoyReader.call_api(self)
-            try:
+            if self.endpoint_type == "PC":
                 production = raw_json["production"][1]["wNow"]
-            except (IndexError, KeyError):
+            else:
                 production = raw_json["wattsNow"]
             return int(production)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "'.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def consumption(self):
-        if self.model == "C":
-            return "Unavailable on model " + self.model
+        if self.endpoint_type == "P":
+            return self.message_consumption_not_available
 
         try:
             raw_json = EnvoyReader.call_api(self)
@@ -61,31 +71,27 @@ class EnvoyReader():
             return int(consumption)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def daily_production(self):
         try:
             raw_json = EnvoyReader.call_api(self)
-            try:
+            if self.endpoint_type == "PC":
                 daily_production = raw_json["production"][1]["whToday"]
-            except (KeyError, IndexError):
+            else:
                 daily_production = raw_json["wattHoursToday"]
             return int(daily_production)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def daily_consumption(self):
-        if self.model == "C":
-            return "Unavailable on model " + self.model
+        if self.endpoint_type == "P":
+            return self.message_consumption_not_available
 
         try:
             raw_json = EnvoyReader.call_api(self)
@@ -93,33 +99,27 @@ class EnvoyReader():
             return int(daily_consumption)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def seven_days_production(self):
         try:
             raw_json = EnvoyReader.call_api(self)
-            try:
-                seven_days_production = raw_json["production"][1][
-                    "whLastSevenDays"
-                ]
-            except (KeyError, IndexError):
+            if self.endpoint_type == "PC":
+                seven_days_production = raw_json["production"][1]["whLastSevenDays"]
+            else:
                 seven_days_production = raw_json["wattHoursSevenDays"]
             return int(seven_days_production)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def seven_days_consumption(self):
-        if self.model == "C":
-            return "Unavailable on model " + self.model
+        if self.endpoint_type == "P":
+            return self.message_consumption_not_available
 
         try:
             raw_json = EnvoyReader.call_api(self)
@@ -127,30 +127,27 @@ class EnvoyReader():
             return int(seven_days_consumption)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def lifetime_production(self):
         try:
             raw_json = EnvoyReader.call_api(self)
-            try:
+            if self.endpoint_type == "PC":
                 lifetime_production = raw_json["production"][1]["whLifetime"]
-            except (KeyError, IndexError):
+            else:
                 lifetime_production = raw_json["wattHoursLifetime"]
             return int(lifetime_production)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "'.")
+            return EnvoyReader.create_json_errormessage(self)
 
     def lifetime_consumption(self):
-        if self.model == "C":
-            return "Unavailable on model " + self.model
+        if self.endpoint_type == "P":
+            return self.message_consumption_not_available
 
         try:
             raw_json = EnvoyReader.call_api(self)
@@ -158,11 +155,9 @@ class EnvoyReader():
             return int(lifetime_consumption)
 
         except requests.exceptions.ConnectionError:
-            return "Unable to connect to Envoy. Check the IP address '" + self.host + "'."
+            return EnvoyReader.create_connect_errormessage(self)
         except (json.decoder.JSONDecodeError, KeyError, IndexError):
-            return ("Got a response, but it doesn't look right. " +
-                    "Check the IP address '" + self.host + "',  or maybe your model of Envoy " +
-                    "doesn't support this.")
+            return EnvoyReader.create_json_errormessage(self)
 
 
 if __name__ == "__main__":

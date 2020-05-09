12206fbe2e9784497fa34a4d23184aec5e8c01ab
Jesse Rizzo <jesse.rizzo@gmail.com>
2020-05-09 10:34:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="envoy_reader",
-    version="0.16.0",
+    version="0.16.1",
     author="Jesse Rizzo",
     author_email="jesse.rizzo@gmail.com",
     description="A program to read from an Enphase Envoy on the local network",

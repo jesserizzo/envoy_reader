0689ffb01da6516fb4b6a28a03c4f9d018a8431d
Greg <greg.diehl.gtd@gmail.com>
2020-12-14 00:42:04 -0800

    Bump version of hotfix to include the fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="envoy_reader",
-    version="0.17.0a",
+    version="0.17.0b",
     author="Jesse Rizzo",
     author_email="jesse.rizzo@gmail.com",
     description="A program to read from an Enphase Envoy on the local network",
